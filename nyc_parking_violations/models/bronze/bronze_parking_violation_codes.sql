58ba4adde7302949f6df5dd8e972f079c976351a
Dhruv Shah <dhruvshah@Dhruvs-MacBook-Pro.local>
2024-03-05 15:40:46 -0500

    Delete additional folders

--- nyc_parking_violations/models/bronze/bronze_parking_violation_codes.sql
+++
@@ -1,8 +0,0 @@
-SELECT code 
-as 
-violation_code, 
-definition,
-manhattan_96th_st_below,
-all_other_areas
-FROM
-parking_violation_codes
--- nyc_parking_violations/models/example/ref_model.sql
+++
@@ -1,1 +0,0 @@
-SELECT COUNT(*) from {{ref('first_model')}}
